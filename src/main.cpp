43454d27f24477600bf442d60388e66f9ca8fc97
ferit <ferit.uzer@valeo.com>
2018-05-05 20:44:56 +0200

    forgotten util functions are cleaned from cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,6 @@ using namespace std;
 // for convenience
 using json = nlohmann::json;
 
-// For converting back and forth between radians and degrees.
-constexpr double pi() { return M_PI; }
-double deg2rad(double x) { return x * pi() / 180; }
-double rad2deg(double x) { return x * 180 / pi(); }
-
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
