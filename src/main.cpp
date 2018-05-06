572a99d83c4914b109d0368ec98ccf9ac9b82eef
ferit <ferit.uzer@valeo.com>
2018-05-06 12:07:15 +0200

    path points are added.

--- src/main.cpp
+++ src/main.cpp
@@ -68,7 +68,9 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  PathPlanner path_planner = PathPlanner(map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy, &path_planner](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -112,6 +114,18 @@ int main() {
 
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+            const unsigned long prev_size = previous_path_x.size();
+            if (prev_size == 0) {
+                path_planner.update(car_x, car_y, car_yaw, car_s, car_s, car_d, sensor_fusion, 0);
+            }
+            else {
+                path_planner.update(car_x, car_y, car_yaw, car_s, end_path_s, car_d, sensor_fusion, prev_size * 0.02);
+            }
+            vector<vector<double>> path_points = path_planner.getPath(previous_path_x, previous_path_y);
+            for (int i = 0; i < path_points[0].size(); i++) {
+                previous_path_x.push_back(path_points[0][i]);
+                previous_path_y.push_back(path_points[1][i]);
+            }
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
